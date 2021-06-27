d78e79073bad19b422f683d35b7f7bbf6cc5ceeb
yutaka <shigure@refy.net>
2021-06-28 02:32:16 +0900

    テーブルドライバ

---
+++ Test/Web/PageObject/Controls/TableItem.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using Selenium.StandardControls;
+using Selenium.StandardControls.PageObjectUtility;
+using Selenium.StandardControls.TestAssistant.GeneratorToolKit;
+
+namespace PageObject.Controls
+{
+    public class TableItem : ComponentBase
+    {
+        public IWebElement label => ByXPath("td[1]").Wait().Find();
+        public CheckBoxDriver checkbox => ByCssSelector("input[type='checkbox']").Wait();
+        public TextBoxDriver text => ByCssSelector("input[type='text']").Wait();
+        public ButtonDriver button => ByTagName("button").Wait();
+
+        public TableItem(IWebElement element) : base(element) { }
+        public static implicit operator TableItem(ElementFinder finder) => finder.Find<TableItem>();
+    }
+}
